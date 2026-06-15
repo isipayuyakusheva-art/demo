84464673fe75e24514c0154b22fc819010926986
isipayuyakusheva-art <isip_a.yu.yakusheva@mpt.ru>
2026-06-15 22:29:06 +0300

    Delete includes directory

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-    </main>
-    <footer class="site-footer">
-        <div class="container">
-            <p>© <?= date('Y') ?> <?= e(cfg('app_name')) ?>. <?= e(cfg('app_tagline')) ?></p>
-        </div>
-    </footer>
-    <script src="assets/js/app.js"></script>
-</body>
-</html>
